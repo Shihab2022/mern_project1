01f3fb3e88f6810edec76e95503ef4933ec05bf8
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-04 08:43:38 +0600

    design login and register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Login/Register';
 import Navbar from './pages/Navbar/Navbar';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
  <Navbar/>
  <Routes>
   <Route path='/' element={<Home/>}></Route>
+  <Route path='/login' element={<Login/>}></Route>
+  <Route path='/register' element={<Register/>}></Route>
  </Routes>
     </div>
   );
